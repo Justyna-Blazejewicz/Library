818db963f956c6674d279f8bf6ae981513f83046
Justyna-Blazejewicz <jusblazejewicz@gmail.com>
2024-03-23 20:52:55 +0100

    Added the possibility to do inputs from the user

---
+++ src/pl/java/library/io/DataReader.java
@@ -0,0 +1,31 @@
+package pl.java.library.io;
+
+import pl.java.library.model.Book;
+
+import java.util.Scanner;
+
+public class DataReader {
+    private Scanner input = new Scanner(System.in);
+
+    public Book readAndCreateBook() {
+        System.out.println("Tytuł");
+        String title = input.nextLine();
+        System.out.println("Autor:");
+        String author = input.nextLine();
+        System.out.println("Wydawnictwo");
+        String publisher = input.nextLine();
+        System.out.println("ISBN:");
+        String isbn = input.nextLine();
+        System.out.println("Rok wydania:");
+        int releaseDate = input.nextInt();
+        input.nextLine();
+        System.out.println("Liczba stron");
+        int pages = input.nextInt();
+        input.nextLine();
+        return new Book(title, author, releaseDate, pages, publisher, isbn);
+    }
+
+    public void close() {
+        input.close();
+    }
+}
