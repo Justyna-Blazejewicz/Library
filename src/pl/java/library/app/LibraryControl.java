41c1668e992328a540449f16e3e7a6221e351062
Justyna-Blazejewicz <jusblazejewicz@gmail.com>
2024-05-06 11:33:52 +0200

    Adding and updating files according to inheritance rules

--- src/pl/java/library/app/LibraryControl.java
+++ src/pl/java/library/app/LibraryControl.java
@@ -3,12 +3,15 @@ package pl.java.library.app;
 import pl.java.library.io.DataReader;
 import pl.java.library.model.Book;
 import pl.java.library.model.Library;
+import pl.java.library.model.Magazine;
 
 public class LibraryControl {
 
     private static final int EXIT = 0;
     private static final int ADD_BOOK = 1;
-    private static final int PRINT_BOOKS = 2;
+    private static final int ADD_MAGAZINE = 2;
+    private static final int PRINT_BOOKS = 3;
+    private static final int PRINT_MAGAZINES = 4;
     private DataReader dataReader = new DataReader();
     private Library library = new Library();
 
@@ -21,8 +24,14 @@ public class LibraryControl {
                 case ADD_BOOK:
                     addBook();
                     break;
+                case ADD_MAGAZINE:
+                    addMagazine();
+                    break;
                 case PRINT_BOOKS:
                     printBooks();
+                    break;
+                case PRINT_MAGAZINES:
+                    printMagazines();
                     break;
                 case EXIT:
                     exit();
@@ -31,6 +40,11 @@ public class LibraryControl {
                     System.out.println("Wybrano niepoprawną opcję. Spróbuj ponownie.");
             }
         } while (option != EXIT);
+    }
+
+    private void addMagazine() {
+        Magazine magazine = dataReader.readAndCreateMagazine();
+        library.addMagazine(magazine);
     }
 
     private void exit() {
@@ -42,6 +56,10 @@ public class LibraryControl {
         library.printBooks();
     }
 
+    private void printMagazines() {
+        library.printMagazines();
+    }
+
     private void addBook() {
         Book book = dataReader.readAndCreateBook();
         library.addBook(book);
@@ -51,7 +69,9 @@ public class LibraryControl {
         System.out.println("Wybierz opcję:");
         System.out.println(EXIT + " - wyjście z programu");
         System.out.println(ADD_BOOK + " - dodanie nowej książki");
+        System.out.println(ADD_MAGAZINE + " - dodanie nowego czasopisma");
         System.out.println(PRINT_BOOKS + " - wyświetl dostępne książki");
+        System.out.println(PRINT_MAGAZINES + " - wyświetl dostępne czasopisma");
     }
 
 }
--- src/pl/java/library/io/DataReader.java
+++ src/pl/java/library/io/DataReader.java
@@ -1,6 +1,7 @@
 package pl.java.library.io;
 
 import pl.java.library.model.Book;
+import pl.java.library.model.Magazine;
 
 import java.util.Scanner;
 
@@ -17,12 +18,29 @@ public class DataReader {
         System.out.println("ISBN:");
         String isbn = input.nextLine();
         System.out.println("Rok wydania:");
-        int releaseDate = input.nextInt();
+        int year = input.nextInt();
         input.nextLine();
         System.out.println("Liczba stron");
         int pages = input.nextInt();
-        input.nextLine();
-        return new Book(title, author, releaseDate, pages, publisher, isbn);
+
+        return new Book(title, author, year, pages, publisher, isbn);
+    }
+
+    public Magazine readAndCreateMagazine() {
+        System.out.print("Tytuł: ");
+        String title = input.nextLine();
+        System.out.print("Wydawnictwo: ");
+        String publisher = input.nextLine();
+        System.out.print("Język: ");
+        String language = input.nextLine();
+        System.out.print("Rok wydania: ");
+        int year = getInt();
+        System.out.print("Miesiąc: ");
+        int month = getInt();
+        System.out.print("Dzień: ");
+        int day = getInt();
+
+        return new Magazine(title, publisher, language, year, month, day);
     }
 
     public int getInt() {
